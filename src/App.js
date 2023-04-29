85d9baa55fd9167af05f20e15fe791684c895d92
Alex <acroce@udel.edu>
2023-04-29 14:30:20 -0400

    added header

--- src/App.js
+++ src/App.js
@@ -6,9 +6,24 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          BreakIn
-        </p>
+        <div className="App">
+            <header className="App-header">
+                <div>
+                  <span className="title">
+                    <div>
+                      <b>Break In</b>
+                    </div>
+                  </span>
+                </div>
+            </header>
+            <div style={{ backgroundColor: "#8096c06e" }}>
+                <div className="buttons">
+                    <div>
+                        <Button>Click Me!</Button>
+                    </div>
+                </div>
+            </div>
+          </div>
       </header>
     </div>
   );
