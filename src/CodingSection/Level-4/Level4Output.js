d36143afbb91caa2556361e86ab657758417fe8f
Alex <acroce@udel.edu>
2023-04-30 11:07:57 -0400

    added map and instructions to level 4

--- src/CodingSection/Level-4/Level4Output.js
+++ src/CodingSection/Level-4/Level4Output.js
@@ -1,7 +1,22 @@
+import classes from "./Level4.module.css";
+
 export const Level4Output = () => {
   return (
     <div>
       <p>This is level4 Output</p>
+      <div className={classes.map}>
+        <div className={classes.mapBox}>
+          <img
+            src={require("../../imgs/prisoner.png")}
+            alt="Ace"
+            className={classes.prisoner_img}
+          />
+        </div>
+        <div className={classes.mapBox}></div>
+        <div className={classes.mapBox}></div>
+        <div className={classes.mapBox}></div>
+        <div className={classes.mapBox}></div>
+      </div>
     </div>
   );
 };
