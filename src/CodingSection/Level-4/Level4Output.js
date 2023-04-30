025da31cf400e4df4c3e71f14788ee32e1e999de
Alex <acroce@udel.edu>
2023-04-30 12:10:59 -0400

    fixed bug crashing level 4

--- src/CodingSection/Level-4/Level4Output.js
+++ src/CodingSection/Level-4/Level4Output.js
@@ -2,41 +2,43 @@ import classes from "./Level4.module.css"
 export const Level4Output = ({corectness}) => {
     if(corectness === true) {
       return(
-    <div>
-      <div className={classes.map}>
-        <div className={classes.mapBox}>
-        </div>
-        <div className={classes.mapBox}></div>
-        <div className={classes.mapBox}></div>
-        <div className={classes.mapBox}></div>
-        <div className={classes.mapBox}>
-         <img
-            src={require("../../imgs/prisoner.png")}
-            alt="Ace"
-            className={classes.prisoner_img}
-          />
+        <div>
+          <div className={classes.map}>
+            <div className={classes.mapBox}>
+            </div>
+            <div className={classes.mapBox}></div>
+            <div className={classes.mapBox}></div>
+            <div className={classes.mapBox}></div>
+            <div className={classes.mapBox}>
+              <img
+                  src={require("../../imgs/prisoner.png")}
+                  alt="Ace"
+                  className={classes.prisoner_img}
+              />
+            </div>
           </div>
-      </div>
-    </div>)
+        </div>
+      )
     }
     else{
       return(
-          <div>
-      <p>This is level4 Output</p>
-      <div className={classes.map}>
-        <div className={classes.mapBox}>
-          <img
-            src={require("../../imgs/prisoner.png")}
-            alt="Ace"
-            className={classes.prisoner_img}
-          />
+        <div>
+          <p>This is level4 Output</p>
+          <div className={classes.map}>
+            <div className={classes.mapBox}>
+              <img
+                src={require("../../imgs/prisoner.png")}
+                alt="Ace"
+                className={classes.prisoner_img}
+              />
+            </div>
+            <div className={classes.mapBox}></div>
+            <div className={classes.mapBox}></div>
+            <div className={classes.mapBox}></div>
+            <div className={classes.mapBox}></div>
+          </div>
         </div>
-        <div className={classes.mapBox}></div>
-        <div className={classes.mapBox}></div>
-        <div className={classes.mapBox}></div>
-        <div className={classes.mapBox}></div>
-      </div>
-    </div>)
+      )
 
     }
 };
