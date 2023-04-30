dc0143369ceb459ce1494c7022f80db165bf992b
Abdulrahman Abdulhamid <abdulrahman.t.abdulhamid@gmail.com>
2023-04-29 21:53:00 -0400

    added navigatoin for each level

--- src/CodingSection/Level-1/Level1.js
+++ src/CodingSection/Level-1/Level1.js
@@ -3,6 +3,7 @@ import { Level1Input } from "./Level1Input";
 import { Level1Modal } from "./Level1Modal";
 import { Level1Output } from "./Level1Output";
 import classes from "./Level1.module.css";
+import { NavigationTitle } from "../Components/NavigationTitle";
 
 export const Level1 = () => {
   const [isModalShown, setIsModalShown] = useState(true);
@@ -12,14 +13,19 @@ export const Level1 = () => {
   };
   return (
     <>
+      <NavigationTitle
+        level="1"
+        right_arrow_dest="/level-2"
+        left_arrow_dest="/"
+      />
       {isModalShown && <Level1Modal hideModal={hideModalHandler} />}
       <Level1Input />
       <Level1Output />
       <div className={classes.container}>
-        <div className={classes.bar} style={{ }}></div>
-        <div className={classes.bar} style={{ }}></div>
-        <div className={classes.bar} style={{ }}></div>
-        <div className={classes.bar} style={{ }}></div>
+        <div className={classes.bar} style={{}}></div>
+        <div className={classes.bar} style={{}}></div>
+        <div className={classes.bar} style={{}}></div>
+        <div className={classes.bar} style={{}}></div>
       </div>
     </>
   );
