6fe7691143e969df54bbb9a7633715127b872303
Alex <acroce@udel.edu>
2023-04-29 21:15:46 -0400

    added a cage

--- src/CodingSection/Level-1/Level1.js
+++ src/CodingSection/Level-1/Level1.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Level1Input } from "./Level1Input";
 import { Level1Modal } from "./Level1Modal";
 import { Level1Output } from "./Level1Output";
-import "./Level1.module.css";
+import classes from "./Level1.module.css";
 
 export const Level1 = () => {
   const [isModalShown, setIsModalShown] = useState(true);
@@ -15,11 +15,11 @@ export const Level1 = () => {
       {isModalShown && <Level1Modal hideModal={hideModalHandler} />}
       <Level1Input />
       <Level1Output />
-      <p>eeeeea</p>
-      <div>
-        <div class="square" style={{ left: "50%" }}></div>
-        <div class="square" style={{ left: "55%" }}></div>
-        <div class="square" style={{ left: "60%" }}></div>
+      <div className={classes.container}>
+        <div className={classes.bar} style={{ }}></div>
+        <div className={classes.bar} style={{ }}></div>
+        <div className={classes.bar} style={{ }}></div>
+        <div className={classes.bar} style={{ }}></div>
       </div>
     </>
   );
